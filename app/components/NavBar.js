8dad2afcbfab9f9d2a43a50a447bb98e28d4a39c
Pinu1623 <pp333090@gmail.com>
2024-01-28 00:23:33 +0530

    second commit

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,37 +1,58 @@
 // Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div className="container-nav">
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-          <Link className='nav-link' activeClass="active" to="home-section" spy={true} smooth={true} offset={50} duration={500}>Home</Link>
-        <Link className='nav-link'
-            activeClass="active" 
-            to="about-section" 
-            spy={true} 
-            smooth={true} 
-            offset={50} 
-            duration={500} 
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+    <nav className={`navbar ${isNavOpen ? 'open' : ''}`}>
+      <div className="container-nav">
+        <div className="navbar-toggle" onClick={toggleNav}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+
+        <div className={`nav-links ${isNavOpen ? 'open' : ''}`}>
+          <Link
+            className='nav-link'
+            activeClass="active"
+            to="home-section"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            Home
+          </Link>
+          <Link
+            className='nav-link'
+            activeClass="active"
+            to="about-section"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
           >
             About Us
           </Link>
-
-          <Link className='nav-link'
-            activeClass="active" 
-            to="contact-section" 
-            spy={true} 
-            smooth={true} 
-            offset={50} 
-            duration={500} 
-            >
-              Contact
-            </Link> 
-
-
-
+          <Link
+            className='nav-link'
+            activeClass="active"
+            to="contact-section"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </div>
       </div>
     </nav>
   );
